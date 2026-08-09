e7819ff2d765ebb6f843a08f0e548f46b6ccaead
Racheli <rachelig1405@gmail.com>
2026-08-09 11:57:01 +0300

    עדכון מערכת

--- server/DB.py
+++ server/DB.py
@@ -271,7 +271,7 @@ def get_orders_filter_by_status(    status: str ,action: int|None=None,user_id: 
     if action==1:
          records = get_all_airtable_records(
         AIRTABLE_ORDERS_TABLE,
-          filter_formula=f'{{בצפי}}=1)',
+          filter_formula='{{בצפי}}=1',
           sort= [
         ("יום עבודה", "asc"),
         ("תאריך אספקה", "asc"),

afac8e3d59c2c2d7ae9863af5c1754be4acbcb19
Racheli <rachelig1405@gmail.com>
2026-09-10 12:38:49 +0300

    loading

--- src/pages/CheckOrder.jsx
+++ src/pages/CheckOrder.jsx
@@ -29,7 +29,7 @@ export default function CheckOrder({ onClose }) {
 
       try {
         const [ordersResponse] = await Promise.all([
-          fetch(`${API_URL}/api/orders/filter_by_status?status=מלוקט`),
+          fetch(`${API_URL}/api/orders/filter_by_status?status=מלוקט&action=0`),
          
         ]);
 
